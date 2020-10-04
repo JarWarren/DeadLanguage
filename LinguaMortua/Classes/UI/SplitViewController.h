c77c120616a55461cafa0755515d58ed6a7c231a
JarWarren <gentlemanjaruna@gmail.com>
2020-10-04 15:08:21 -0600

    add splitviewcontroller

---
+++ LinguaMortua/Classes/UI/SplitViewController.h
@@ -0,0 +1,17 @@
+//
+//  SplitViewController.h
+//  LinguaMortua
+//
+//  Created by Jared Warren on 10/4/20.
+//  Copyright © 2020 Warren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SplitViewController : UISplitViewController <UISplitViewControllerDelegate>
+
+@end
+
+NS_ASSUME_NONNULL_END
